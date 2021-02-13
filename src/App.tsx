5b4eea40ea5366d54f93f8c3431bb498b3a6942b
Srishailam <sriyd531@gmail.com>
2021-02-13 02:02:38 -0500

    added apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider} from 'react-apollo';
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './Header';
+import RepoSearch from "./RepoSearch";
 
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers:{
+    authorization: `Bearer 2a2399c35455007d3466e1dd24e4b48e28001dce`
+  }
+});
 
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <Header/>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <Header/>
+          </header>
+          <RepoSearch client={client}/>
+        </div>
+      </ApolloProvider>
     );
   }
 }
